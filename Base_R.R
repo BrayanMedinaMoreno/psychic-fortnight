735f69a4d4a6953b15e5316a15be9d68de87ff21
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-07-14 10:11:24 -0500

    Si

---
+++ Base_R.R
@@ -0,0 +1,123 @@
+# SUMA
+
+50000 + 10000
+
+
+# RESTA
+
+112000 - 50000
+
+
+# MULTIPLICACION
+
+2000 * 122
+
+
+# DIVISION
+
+3222 / 12
+
+
+# JERARQUIA DE OPERACIONES
+
+(4 * 6) + 2
+
+4 * (6 + 2)
+
+4 * 2 + 6
+
+4 + 3 * 5 / 7.5
+
+
+# DIVISION ENTERA
+
+24 %/% 5
+
+
+# MODULO
+
+15 %% 4
+
+17 %% 4
+
+# LOGICA
+
+TRUE & TRUE
+
+TRUE & FALSE 
+
+FALSE & FALSE 
+
+TRUE | TRUE
+
+TRUE | FALSE
+
+FALSE | FALSE
+
+
+# VARIABLES
+
+
+X <- 4
+X <- 78
+
+
+X <- "MUcho Texto"
+y <- "Holi"
+
+
+a <- 54
+a <- 54
+
+
+a <- b <- 54
+
+
+assign("j",567)
+
+
+exists("a")
+
+exists("c")
+
+exists("a")
+
+
+rm(x)
+
+
+rm(list = ls ())
+
+
+#TIPOS DE DATOS
+# NUMERICO
+
+x <- 12
+
+class(x)
+
+#STRING
+
+y <- "ola"
+
+class(y)
+
+#BOOLEAN
+
+g <- FALSE
+
+class(g)
+
+#FACTOR
+
+x <- factor("hombre")
+
+class(X)
+
+#DATE
+
+fecha <- as.Date("2012-02-14")
+class(fecha)
+
+is.factor(x)
+is.character(y)
